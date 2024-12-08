44952b5125e7008212bc7ea921d3315a6393ddf4
scenlinx <445782918@qq.com>
2024-12-08 23:08:21 +0800

    更新 socialLinks.ts

--- .vitepress/configs/socialLinks.ts
+++ .vitepress/configs/socialLinks.ts
@@ -4,24 +4,7 @@ import type { DefaultTheme } from 'vitepress'
 export const socialLinks: DefaultTheme.SocialLink[] = [
   {
     icon: 'github',
-    link: 'https://github.com/Theo-Messi/Theo-Docs',
+    link: 'https://github.com/scenlinx',
     ariaLabel: 'Github'
   },
-  {
-    icon: 'gmail',
-    link: 'mailto:fanxiaobin422@gmail.com',
-    ariaLabel: 'Gmail'
-  },
-  {
-    icon: 'telegram',
-    link: 'https://t.me/Theo_Messi',
-    ariaLabel: 'Telegram'
-  },
-  {
-    icon: {
-      svg: '<iconify-icon icon="twemoji:red-heart" style="margin-right: 0.125em; margin-bottom: 0.125rem; font-size:1.2em" alt="Github Sponsors"></iconify-icon>'
-    },
-    link: 'https://github.com/sponsors/Theo-Messi',
-    ariaLabel: 'Github Sponsors'
-  }
 ]
