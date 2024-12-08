edb9039cdff7155cd3eeec2fa5efcd0f6eb17c60
scenlinx <445782918@qq.com>
2024-12-08 22:03:14 +0800

    1

---
+++ .vitepress/configs/socialLinks.ts
@@ -0,0 +1,27 @@
+import type { DefaultTheme } from 'vitepress'
+// 社交链接设置
+
+export const socialLinks: DefaultTheme.SocialLink[] = [
+  {
+    icon: 'github',
+    link: 'https://github.com/Theo-Messi/Theo-Docs',
+    ariaLabel: 'Github'
+  },
+  {
+    icon: 'gmail',
+    link: 'mailto:fanxiaobin422@gmail.com',
+    ariaLabel: 'Gmail'
+  },
+  {
+    icon: 'telegram',
+    link: 'https://t.me/Theo_Messi',
+    ariaLabel: 'Telegram'
+  },
+  {
+    icon: {
+      svg: '<iconify-icon icon="twemoji:red-heart" style="margin-right: 0.125em; margin-bottom: 0.125rem; font-size:1.2em" alt="Github Sponsors"></iconify-icon>'
+    },
+    link: 'https://github.com/sponsors/Theo-Messi',
+    ariaLabel: 'Github Sponsors'
+  }
+]
--- docs/home/MFriends.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { FRIEND_DATA } from './friend-data'
-</script>
-
-<template>
-  <div class="pt-8">
-    <MNavLinks v-bind="FRIEND_DATA" />
-  </div>
-</template>
