7c927991b8a58a58bb2dc73e444d7c83f84586ae
scenlinx <445782918@qq.com>
2024-12-09 16:02:28 +0800

    Update nav.ts

--- .vitepress/configs/nav.ts
+++ .vitepress/configs/nav.ts
@@ -13,14 +13,12 @@ export const nav: DefaultTheme.NavItem[] = [
     activeMatch: '^/sources/'
   },
   {
-    text: '码农专区',
-    link: '/it-serve/',
-    activeMatch: '^/it-serve/'
-  },
-  {
-    text: '玄学中医',
-    link: '/classics/',
-    activeMatch: '^/classics/'
+    text: '四海闲谈',
+    items: [
+      { text: '码农专区', link: '/it-serve/index', activeMatch: '^/it-serve/' },
+      { text: '玄学中医', link: '/classics/index', activeMatch: '^/classics/' },
+    ],
+    activeMatch: '/it-serve/'
   },
   { text: '网盘资源', link: 'https://pan.newmt.fun/' },
   { text: '导航', link: 'https://nav.newmt.fun/' }
