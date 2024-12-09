9f040921e109fd6a20e13c2fddae31ced85cf26a
scenlinx <445782918@qq.com>
2024-12-09 15:18:02 +0800

    1

--- .vitepress/theme/types/types.ts
+++ .vitepress/theme/types/types.ts
@@ -151,3 +151,13 @@ export interface VideoConfig {
   /** 视频播放器的标题 */
   title: string
 }
+export interface NavLink {
+  /** 站点图标 */
+  icon?: string | { svg: string }
+  /** 站点名称 */
+  title: string
+  /** 站点名称 */
+  desc?: string
+  /** 站点链接 */
+  link: string
+}
