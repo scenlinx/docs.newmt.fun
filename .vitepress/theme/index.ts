225b6febcd02334d05456c3e48323adff6e58d9c
scenlinx <445782918@qq.com>
2025-01-21 22:31:24 +0800

    1

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,10 +4,6 @@ import { h } from 'vue'
 
 import imageViewer from 'vitepress-plugin-image-viewer'
 import {
-  DocBox,
-  DocLinks,
-  DocBoxCube,
-  DocVideoLink,
   HomeFooter,
   backtotop
 } from './types/index.js'
@@ -27,10 +23,6 @@ export default {
     })
   },
   enhanceApp: ({ app }) => {
-    app.component('Box', DocBox)
-    app.component('Links', DocLinks)
-    app.component('BoxCube', DocBoxCube)
-    app.component('Vid', DocVideoLink)
     app.component('BackTop', backtotop)  // 注册 backtotop 组件
   },
   setup() {
