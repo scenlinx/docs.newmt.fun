92f3cf0682cab992e334f2502c73a615fd07e0b7
it001 <445782918@qq.com>
2025-05-20 17:00:38 +0800

    Update nav.ts

--- .vitepress/configs/nav.ts
+++ .vitepress/configs/nav.ts
@@ -13,32 +13,32 @@ export const nav: DefaultTheme.NavItem[] = [
     activeMatch: '^/moneys/'
   },
   {
-    text: '<iconify-icon class="i-mr" icon="heroicons:archive-box-solid" style="color:#20c997"></iconify-icon>资源合集',
+    text: '<iconify-icon class="i-mr" icon="heroicons:archive-box-solid" style="color:#fd7e14"></iconify-icon>资源合集',
     link: '/sources/',
     activeMatch: '^/sources/'
   },
   {
-    text: '<iconify-icon class="i-mr" icon="heroicons:chat-bubble-left-ellipsis-solid" style="color:#20c997"></iconify-icon>四海闲谈',
+    text: '<iconify-icon class="i-mr" icon="heroicons:chat-bubble-left-ellipsis-solid" style="color:#e83e8c"></iconify-icon>四海闲谈',
     items: [
       { 
-        text: '<iconify-icon class="i-mr" icon="heroicons:code-bracket-solid" style="color:#20c997"></iconify-icon>码农专区', 
+        text: '<iconify-icon class="i-mr" icon="heroicons:code-bracket-solid" style="color:#007bff"></iconify-icon>码农专区', 
         link: '/it-serve/index', 
         activeMatch: '^/it-serve/' 
       },
       { 
-        text: '<iconify-icon class="i-mr" icon="heroicons:sparkles-solid" style="color:#20c997"></iconify-icon>玄学中医', 
+        text: '<iconify-icon class="i-mr" icon="heroicons:sparkles-solid" style="color:#6f42c1"></iconify-icon>玄学中医', 
         link: '/classics/index', 
         activeMatch: '^/classics/' 
       },
     ],
-    activeMatch: '^/it-serve/'
+    activeMatch: '/it-serve/'
   },
   { 
-    text: '<iconify-icon class="i-mr" icon="heroicons:cloud-arrow-down-solid" style="color:#20c997"></iconify-icon>网盘资源', 
+    text: '<iconify-icon class="i-mr" icon="heroicons:cloud-arrow-down-solid" style="color:#17a2b8"></iconify-icon>网盘资源', 
     link: 'https://pan.newmt.fun/' 
   },
   { 
-    text: '<iconify-icon class="i-mr" icon="heroicons:map-solid" style="color:#20c997"></iconify-icon>导航', 
+    text: '<iconify-icon class="i-mr" icon="heroicons:map-solid" style="color:#28a745"></iconify-icon>导航', 
     link: 'https://nav.newmt.fun/' 
   }
 ]
